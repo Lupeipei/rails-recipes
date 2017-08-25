5b64ee537f46dd096884608396aa548f9d2837dc
Lucia <learningleadtorebirth@gmail.com>
2017-08-25 22:45:39 +0800

    add profile to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :find_user
 
   def edit
-    @user = current_user
+  end
+
+  def show
   end
 
   def update
-    @user = current_user
+
     if @user.update(user_params)
       redirect_to edit_user_path, notice: "updated"
     else
@@ -17,7 +20,12 @@ class UsersController < ApplicationController
 
   protected
 
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
+
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :sepciality])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ApplicationRecord
 
   has_many :memberships
   has_many :groups, :through => :memberships
+  has_one :profile
+  accepts_nested_attributes_for :profile
 
 end

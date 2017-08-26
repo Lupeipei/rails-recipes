ca911f73b24324a02ce266bf229e9708a03ab84b
Lucia <learningleadtorebirth@gmail.com>
2017-08-26 16:09:41 +0800

    fixed registration.rb

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,22 @@
 class Registration < ApplicationRecord
-  belongs_to :ticket, :optional => true
+
+  STATUS = ["pending", "confirmed"]
+  validates_inclusion_of :status, :in => STATUS
+  validates_presence_of :status, :ticket_id
+
+  belongs_to :ticket
+  belongs_to :user, :optional => true
+  belongs_to :event
+
+  before_validation :generate_uuid, :on => :create
+
+  def to_param
+    self.uuid
+  end
+
+  protected
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
